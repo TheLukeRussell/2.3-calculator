d900957fea6f63a3572b91bca5c2a215f10d88b0
Luke Russell <thelukerussel21@gmail.com>
2020-01-17 16:18:04 -0500

    fixed button function

--- js/main.js
+++ js/main.js
@@ -1,56 +1,86 @@
 // alert("hello");
 // const calculator = document.querySelector('.calculator');
 // const buttons = document.querySelector('.calculator-buttons');
-// const calculator = { displayValue: '0', firstOperand: null, waitingForSecondOperand: false, operator: null, };
-// const buttons = document.querySelectorAll('button');
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
+const buttons = document.querySelectorAll('button');
 // const numberButton = document.querySelectorAll('.digit');
 // const operationButton = document.querySelectorAll('.operator');
 // const equalsButton = document.querySelector('.data-equals');
 // const allClearButton = document.querySelector('.data-all-clear');
 // const plusMinusButton = document.querySelector('.data-plus-minus');
 
-// function updateDisplay() {
-//   const display = document.querySelector('#results_bar');
-//   display.value = calculator.displayValue;
+function updateDisplay() {
+  const display = document.querySelector('#calculator-screen');
+  display.value = calculator.displayValue;
+}
+//
+updateDisplay();
+
+// for(let t=0, i < $buttons.length; i = i + 1){
+//   buttons[i].addEventListener('click', () => {
+//     console.log('event', event)
+//   });
 // }
-
-// updateDisplay();
-
-// for(let i=0, i < buttons.length; i = i+ 1){
-//   buttons[i].addEventListener
-// }
-
+//
 // for(let i = 0; i < buttons.length; i++) {
 // console.log(buttons[i])
 // }
-// const keys = document.querySelector('.calculator-buttons');
-const keys = document.querySelector('.calculator-buttons');
-keys.addEventListener('click', (event) => {
-  const { target } = event;
-  if (!target.matches('button')) {
-    return;
-  }
 
-  if (target.classList.contains('operator')) {
-    console.log('operator', target.value);
-    return;
-  }
+const numbers = document.querySelectorAll('.digit');
+// console.log('numbers', numbers);
 
-  if (target.classList.contains('decimal')) {
-    console.log('decimal', target.value);
-    return;
-  }
 
-  if (target.classList.contains('all-clear')) {
-    console.log('clear', target.value);
-    return;
-  }
+// keys.addEventListener('click', (event) => {
+//     (event.target.classList.contains('operator'))
+//     console.log('operator', event.target.value);
+//     // alert(`you pressed ${event.target.value}`);
+//     return;
+//
+//   });
+//
+// keys.addEventListener('click', (event) => {
+//     (event.target.classList.contains('decimal'))
+//     console.log('decimal', event.target.value);
+//     // alert(`you pressed ${event.target.value}`);
+//     return;
+//
+// });
+// keys.addEventListener('click', (event) => {
+//     (event.target.classList.contains('all-clear'))
+//     console.log('clear', event.target.value);
+//     // alert(`you pressed ${event.target.value}`);
+//     return;
+//
+// });
 
-  if (target.classList.contains('digit')) {
-    console.log('digit', target.value);
-    return;
-  }
-});
+for(let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener('click', (event) => {
+      // (event.target.classList.contains('digit'))
+      console.log('digit', event.target.value);
+      // alert(`you pressed ${event.target.value}`);
+      return;
+  });
+}
+
+
+
+///////IN CLASS DEMO//////
+// for(let i = 0; i < buttons.length; i++) {
+// buttons[i].addEventListener('click', (event) => {
+//   console.log('value', event.target.value);
+// });
+// }
+//
+// let pushNumber = (event) => {
+//   console.log('you pressed the number $event.target.value');
+// }
+///////IN CLASS DEMO////////
+
 
 //convert number to string// Number('123') = 123
 //
